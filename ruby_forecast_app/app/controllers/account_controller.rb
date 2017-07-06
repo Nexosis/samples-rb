8550f68614121b811d57f3ec4dcb6d58ddab2352
Jeff Gabriel <jeffgabriel@outlook.com>
2017-07-06 13:50:01 -0400

    add support for S3 file upload

--- ruby_forecast_app/app/controllers/account_controller.rb
+++ ruby_forecast_app/app/controllers/account_controller.rb
@@ -5,7 +5,7 @@ class AccountController < ApplicationController
 	def index
 		params.permit(:uploaded)
 		if(params["uploaded"])
-			@uploadMessage = "Your dataset has been uploaded."
+			@uploadMessage = "Your dataset has been uploaded. If you used S3 it may take a moment before you see the dataset below."
 		end
 
 		@account_balance = @api_client.get_account_balance
@@ -25,18 +25,26 @@ class AccountController < ApplicationController
 	end
 
 	def upload
-		uploaded_io = params["datafile"]
+		useS3 = params["fileLocation"] == "S3"
 		dataset_name = params["dataset_name"]
-		File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
-			file.write(uploaded_io.read)
-		end
-		csv = CSV.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'rb', headers: true)
-		begin
-			@api_client.create_dataset_csv(dataset_name, csv)
-		rescue NexosisApi::HttpException => http_error
-			@error_message = http_error.message
-			render
-			return
+		if(useS3)
+			bucket = params["bucket"]
+			path = params["path"]
+			region = params["region"]
+			@api_client.import_from_s3(dataset_name,bucket,path,region)
+		else
+			uploaded_io = params["datafile"]
+			File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+				file.write(uploaded_io.read)
+			end
+			csv = CSV.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'rb', headers: true)
+			begin
+				@api_client.create_dataset_csv(dataset_name, csv)
+			rescue NexosisApi::HttpException => http_error
+				@error_message = http_error.message
+				render
+				return
+			end
 		end
 		redirect_to action: 'index', :uploaded => true
 	end
