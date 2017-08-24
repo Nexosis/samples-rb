e16839342bb0a533c1e7a1552212277e977acf47
Jeff Gabriel <gabe19@yahoo.com>
2017-08-24 11:27:43 -0400

    knuckles whacked by rubyist. fixed casing bug in results

--- ruby_forecast_app/app/controllers/results_controller.rb
+++ ruby_forecast_app/app/controllers/results_controller.rb
@@ -8,47 +8,50 @@ class ResultsController < ApplicationController
       @datasets = @api_client.list_datasets
     end
     
-    @sessionResult = @api_client.get_session_results(params[:sessionId]);
-    is_dataset = @datasets.map(&:dataset_name).include? @sessionResult.datasource_name
+    @session_result = @api_client.get_session_results(params[:sessionId]);
+    is_dataset = @datasets.map(&:dataset_name).include? @session_result.datasource_name
     #TODO: cannot determine pages from API - need dataset information to be more specific
-    if @sessionResult.type == "forecast"
-      #timestamp_col = @sessionResult.column_metadata.select{|dc| dc.role == NexosisApi::ColumnRole::TIMESTAMP}.take(1).first().name
-      firstPred = Date.parse(@sessionResult.startDate)
-      lastObs =  firstPred - calc_day_interval(@sessionResult.result_interval,1)
-      firstObs = firstPred - calc_day_interval(@sessionResult.result_interval,30)
+    if @session_result.type == "forecast"
+      #timestamp_col = @session_result.column_metadata.select{|dc| dc.role == NexosisApi::ColumnRole::TIMESTAMP}.take(1).first().name
+      firstPred = Date.parse(@session_result.startDate)
+      lastObs =  firstPred - calc_day_interval(@session_result.result_interval,1)
+      firstObs = firstPred - calc_day_interval(@session_result.result_interval,30)
       if is_dataset
-        @observations = @api_client.get_dataset(@sessionResult.datasource_name, 0, 30, {:start_date => firstObs, :end_date => lastObs})
+        @observations = @api_client.get_dataset(@session_result.datasource_name, 0, 30, {:start_date => firstObs, :end_date => lastObs})
       else
-        @observations = @api_client.get_view(@sessionResult.datasource_name, 0, 30, {:start_date => firstObs, :end_date => lastObs})
+        @observations = @api_client.get_view(@session_result.datasource_name, 0, 30, {:start_date => firstObs, :end_date => lastObs})
       end
     else
-      firstObs = Date.parse(@sessionResult.startDate) - 15
-      lastObs = Date.parse(@sessionResult.endDate) + 15
-      totalEventDays = (Date.parse(@sessionResult.endDate) - Date.parse(@sessionResult.startDate)).to_i
+      firstObs = Date.parse(@session_result.startDate) - 15
+      lastObs = Date.parse(@session_result.endDate) + 15
+      totalEventDays = (Date.parse(@session_result.endDate) - Date.parse(@session_result.startDate)).to_i
       if is_dataset
-        @observations = @api_client.get_dataset(@sessionResult.datasource_name, 0, totalEventDays + 30, {:start_date => firstObs, :end_date => lastObs})
+        @observations = @api_client.get_dataset(@session_result.datasource_name, 0, totalEventDays + 30, {:start_date => firstObs, :end_date => lastObs})
       else
-        @observations = @api_client.get_view(@sessionResult.datasource_name, 0, totalEventDays + 30, {:start_date => firstObs, :end_date => lastObs})
+        @observations = @api_client.get_view(@session_result.datasource_name, 0, totalEventDays + 30, {:start_date => firstObs, :end_date => lastObs})
       end
     end
-    arr = []
     #HACK: need to make sure can reference target no matter how specified session, datasset, view
-    @observations.data.each do |data_hash|
+    @observations.data = @observations.data.map do |data_hash|
       new_hash = {}
       data_hash.each { |k, v| new_hash.store k.downcase, v }
-      arr << new_hash
+      new_hash
     end
-    @observations.data = arr
+    @session_result.data = @session_result.data.map do |data_hash|
+      new_hash = {}
+      data_hash.each { |k, v| new_hash.store k.downcase, v }
+      new_hash
+    end
     render
   end
 
   def file
     params.require(:sessionId)
     client = NexosisApi.client(Rails.application.secrets.api_key)
-    @sessionResult = client.get_session_results(params[:sessionId], true);
+    @session_result = client.get_session_results(params[:sessionId], true);
     filename = "#{params[:sessionId]}.csv"
     response.headers['Content-Disposition'] = "attachment;filename=#{filename}"
-    render inline: @sessionResult, content_type: 'text/csv'
+    render inline: @session_result, content_type: 'text/csv'
   end
 
   private
